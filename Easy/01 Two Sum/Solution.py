501e52a804d14d332febea888bd2c5d814a0acea
Julian Giudice <54405665+karpo27@users.noreply.github.com>
2023-06-08 23:08:12 -0300

    Create Solution.py

---
+++ Easy/01 Two Sum/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def two_sum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {}
+        for index, num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement], index]
+            num_dict[num] = index
+        return []
+      
